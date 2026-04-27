2de905b2b6b1eb52044b44ca6b9603af5a313970
upload-source <puzzle-lend-squash@duck.com>
2026-04-27 14:18:06 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
+export USE_CCACHE=1
+export CCACHE_DIR=/builds/votre-projet/ccache
+ccache -M 50G
 sudo apt update
-sudo apt install -y sudo git android-sdk-platform-tools python-is-python3 python3-yaml qemu-utils  bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick protobuf-compiler python3-protobuf lib32readline-dev lib32z1-dev libdw-dev libelf-dev lz4 libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev meson-1.5 glslang-tools python3-mako
+sudo apt install -y sudo git android-sdk-platform-tools python-is-python3 python3-yaml qemu-utils # libncurses5
+sudo apt install -y bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick protobuf-compiler python3-protobuf lib32readline-dev lib32z1-dev libdw-dev libelf-dev lz4 libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
+sudo apt install -y meson-1.5 glslang-tools python3-mako
 git config --global user.name "github-actions[bot]"
 git config --global user.email "github-actions[bot]@users.noreply.github.com"
 git config --global trailer.changeid.key "Change-Id"
@@ -15,13 +20,11 @@ export PATH="$(realpath .)/bin:$PATH"
 cd android/lineage
 export PATH="$(realpath .)/prebuilts/sdk/tools/linux/bin/:$PATH"
 repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs --no-clone-bundle
-repo sync -c -j 8 # $(nproc)
+repo sync -j 8 # $(nproc)
 
 source build/envsetup.sh
 export AB_OTA_UPDATER=true ROOMSERVICE_BRANCHES="lineage-23.1 lineage-23.0"
-breakfast virtio_arm64only_go
 echo "CONFIG_RTC_CLASS=y" >> kernel/virt/virtio/arch/arm64/configs/lineageos/virtio.config
 echo "CONFIG_ALARMTIMER=y" >> kernel/virt/virtio/arch/arm64/configs/lineageos/virtio.config
-m recoveryimage
-breakfast virtio_arm64only user
-m vm-utm-zip otapackage
+breakfast virtio_arm64only user 
+m vm-utm-zip
